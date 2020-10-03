23ec93bad77e94e2323f2148df012ad524660851
WenrrrBeth <bethding0923@gmail.com>
2020-10-03 15:03:56 -0600

    shape added

--- app/src/main/java/com/example/cmput301f20t22/Shape.java
+++ app/src/main/java/com/example/cmput301f20t22/Shape.java
@@ -3,6 +3,9 @@ package com.example.cmput301f20t22;
 public abstract class Shape {
     int x;
     int y;
+<<<<<<< HEAD
     String color = "White";
+=======
+>>>>>>> f586b5443c37ce9109a99238bffc51b4f27ce2a4
 
 }
